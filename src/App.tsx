ec07997a00e130feded28ee4d17cf14f29b6052e
Ola Ishola <olaishola@hotmail.co.uk>
2023-02-17 21:20:58 +0100

    Install MUI and react-router-dom

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+
+function App() {
+  return (
+    <div style={{ "fontFamily": 'Inter, sans-serif' }}>
+      <CssBaseline />
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
