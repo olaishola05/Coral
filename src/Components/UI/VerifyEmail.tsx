fe79d6954184013b1347541e022284ab52e45b47
Ola Ishola <olaishola@hotmail.co.uk>
2023-02-24 21:23:44 +0100

    Add image to email verify component

--- src/Components/UI/VerifyEmail.tsx
+++ src/Components/UI/VerifyEmail.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import HeaderText from './HeaderText'
+import SuccessImg from '../../Assets/svg/mark.svg'
 
 const VerifyEmail = () => {
   const text = (<span>Check your email inbox.<br />We sent you a confirmation email.</span>)
@@ -8,7 +9,9 @@ const VerifyEmail = () => {
       <HeaderText
         header='Your workspace is ready'
         text={text}
+        headerStyle={{ marginBottom: '50px' }}
       />
+      <img src={SuccessImg} alt={SuccessImg} style={{ width: '135px', height: '135' }} />
     </div>
   )
 }
