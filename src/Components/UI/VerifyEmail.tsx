40b0c5de1d38bca264a6d4f08b48315e9d0ed8b8
Oladipupo Ishola <olaishola@hotmail.co.uk>
2023-06-05 20:09:05 +0100

    refactor(app): Add mobile to verify email
    Add mobile responsive to verify email component

--- src/Components/UI/VerifyEmail.tsx
+++ src/Components/UI/VerifyEmail.tsx
@@ -1,18 +1,30 @@
 import React from 'react'
 import HeaderText from './HeaderText'
 import SuccessImg from '../../Assets/svg/mark.svg'
+import { styled } from '@mui/material/styles'
+
+const StyledContainer = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'column',
+
+  [theme.breakpoints.down('sm')]: {
+    width: '100%',
+    padding: '10px 20px',
+  },
+}))
 
 const VerifyEmail = () => {
   const text = (<span>Check your email inbox.<br />We sent you a confirmation email.</span>)
   return (
-    <div>
+    <StyledContainer>
       <HeaderText
         header='Your workspace is ready'
         text={text}
         headerStyle={{ marginBottom: '50px' }}
       />
       <img src={SuccessImg} alt={SuccessImg} style={{ width: '135px', height: '135' }} />
-    </div>
+    </StyledContainer>
   )
 }
 
--- src/Pages/Signup/EmailVerify.tsx
+++ src/Pages/Signup/EmailVerify.tsx
@@ -7,8 +7,6 @@ import VerifyEmail from '../../Components/UI/VerifyEmail'
 const EmailVerify = () => {
   return (
     <PageLayoutSplit
-      leftClassName={{ width: '622px', padding: '70px 100px 100px' }}
-      rightStyles={{ width: '100%', backgroundColor: '#F5F5F5' }}
       leftSide={<VerifyEmail />}
       rightSide={<ImageContainer src={signupImage} alt={signupImage} />}
     />
